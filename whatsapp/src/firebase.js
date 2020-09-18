ff7bd00543e0429755aff48bc463ea1ee3c5dbcd
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-09-17 23:58:03 -0400

    Firebase config

---
+++ whatsapp/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBXYMuJJn_DihRTqQwZt6MwvMQZgtv3x2Q",
+  authDomain: "whatsapp-8f390.firebaseapp.com",
+  databaseURL: "https://whatsapp-8f390.firebaseio.com",
+  projectId: "whatsapp-8f390",
+  storageBucket: "whatsapp-8f390.appspot.com",
+  messagingSenderId: "264321499821",
+  appId: "1:264321499821:web:3b3c485ed8aa75015c7c24",
+};
