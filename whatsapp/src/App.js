b060b64db803a617b95a36954c951152a8982633
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-09-18 00:02:21 -0400

    Sidebar componenet foundation w/ css

--- whatsapp/src/App.js
+++ whatsapp/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import Sidebar from "./sidebar.js";
 
 function App() {
   return (
     <div className="app">
       <h2>Lets build whatsapp</h2>
+      <Sidebar />
+      {/* Chat */}
     </div>
   );
 }
---
+++ whatsapp/src/sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./sidebar.css";
+function Sidebar() {
+  return (
+    <div>
+      <h1>Sidebar</h1>
+    </div>
+  );
+}
+
+export default Sidebar;
