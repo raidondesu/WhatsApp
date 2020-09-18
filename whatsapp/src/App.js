c882a2ef5cdde7576539476a1ea64c3419c74993
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-09-18 00:03:53 -0400

    Chat component foundation w/ css

--- whatsapp/src/App.js
+++ whatsapp/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
 import Sidebar from "./sidebar.js";
+import Chat from "./chat.js";
 
 function App() {
   return (
     <div className="app">
       <h2>Lets build whatsapp</h2>
       <Sidebar />
-      {/* Chat */}
+      <Chat />
     </div>
   );
 }
