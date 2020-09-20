42a208461b6dcd2ca85034a43897fbcae43e2489
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-09-20 00:14:13 -0400

    Creating sections for sidebar header

--- whatsapp/src/sidebar.js
+++ whatsapp/src/sidebar.js
@@ -4,6 +4,9 @@ function Sidebar() {
   return (
     <div className="sidebar">
       <h1>Sidebar</h1>
+      <div className="sidebar__header">
+        <div className="sidebar__headerRight"></div>
+      </div>
     </div>
   );
 }
