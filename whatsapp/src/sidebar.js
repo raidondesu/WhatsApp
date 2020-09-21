55392489abd0417a2b4048771c4ae3052cccede1
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-09-21 10:42:27 -0400

    Button Functionality to Donut Icon

--- whatsapp/src/sidebar.js
+++ whatsapp/src/sidebar.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./sidebar.css";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import { Avatar, IconButton } from "@material-ui/core";
 function Sidebar() {
   return (
     <div className="sidebar">
       <h1>Sidebar</h1>
       <div className="sidebar__header">
         <div className="sidebar__headerRight">
-          <DonutLargeIcon />
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
         </div>
       </div>
     </div>
