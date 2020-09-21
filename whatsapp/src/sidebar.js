8b2e18a72683146965c6bb284343499989a8e3d1
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-09-21 10:45:49 -0400

    Added chat and more vert icon with functionality

--- whatsapp/src/sidebar.js
+++ whatsapp/src/sidebar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./sidebar.css";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ChatIcon from "@material-ui/icons/Chat";
 import { Avatar, IconButton } from "@material-ui/core";
 function Sidebar() {
   return (
@@ -10,6 +12,14 @@ function Sidebar() {
         <div className="sidebar__headerRight">
           <IconButton>
             <DonutLargeIcon />
+          </IconButton>
+
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+
+          <IconButton>
+            <MoreVertIcon />
           </IconButton>
         </div>
       </div>
