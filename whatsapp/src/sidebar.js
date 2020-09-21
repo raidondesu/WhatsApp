33307ddc648acdae58bc5654ba74a629a133ffa5
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-09-21 10:38:13 -0400

    Material UI Icons dependancy and Large Donut Icon

--- whatsapp/src/sidebar.js
+++ whatsapp/src/sidebar.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./sidebar.css";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 function Sidebar() {
   return (
     <div className="sidebar">
       <h1>Sidebar</h1>
       <div className="sidebar__header">
-        <div className="sidebar__headerRight"></div>
+        <div className="sidebar__headerRight">
+          <DonutLargeIcon />
+        </div>
       </div>
     </div>
   );
