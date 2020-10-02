140000ca766d515d08baa4a10aeb09407a61007d
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-10-01 23:23:44 -0400

    Foundation of REST api on server.js

--- whatsapp/backend/server.js
+++ whatsapp/backend/server.js
@@ -1,7 +1,9 @@
 // imports
+import express from "express";
 
 // config
-
+const app = express();
+const port = process.env.PORT || 9000;
 // middleware
 
 // Database
@@ -9,5 +11,6 @@
 //
 
 // API
-
+app.get("/", (req, res) => res.status(200).send("hello world"));
 // listener
+app.listen(port, () => console.log(`Listening on localhost:${port}`));
